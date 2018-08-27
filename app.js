2dd5eaf87760d46cb1dcea2f4f98d66c4e214bb9
WeicoMY <470398956@qq.com>
2018-08-28 00:03:24 +0800

    change to use router module

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express')
 var bodyParser = require('body-parser')
-
+var router = require('./src/js/router')
 var app = express()
 
 // app调用其他模块
@@ -12,10 +12,7 @@ app.use(bodyParser.json())
 app.use('/src/',express.static('./src/'))
 
 // 引入路由
-app.get('/', function (req, res) {
-  res.render('index.html')
-})
-
+app.use(router)
 app.listen(3000, function (err) {
   if (err) {
     console.log('Can not open the file')
